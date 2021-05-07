d3efff932f08c898c3f1cdd24effb3490a5968f0
drisko7878 <drisko548@protonmail.com>
2021-05-07 21:02:51 +0200

    funckne metody create a use (OVERENE!) pocetPremmenych sa spravne pocita -len treba dorobit podmienku pre -1

--- src/BDD.java
+++ src/BDD.java
@@ -16,11 +16,13 @@ public class BDD {
     public BDD BDD_create(String input){//argument moze byt string
         int cisloSize = input.length();
 
-        BDD bdd = new BDD();
-       // bdd.root = new Node(input);
+        int height = -1;
 
-        bdd.root = createNode(root = new Node(input), input);
-        pocetUzlov++;
+        BDD bdd = new BDD();
+        // bdd.root = new Node(input);
+
+        bdd.root = createNode(root = new Node(input), input, bdd, height);
+        bdd.pocetUzlov++;
 
 
         return bdd;
@@ -28,24 +30,30 @@ public class BDD {
     }
 
 
-    private Node createNode(Node node, String input) {
+    private Node createNode(Node node, String input, BDD bdd, int height) {
+
+        height++;
+        if(bdd.pocetPremennych < height){
+            bdd.pocetPremennych = height;
+        }
+
 
         if(node == null){
-            //pocetUzlov++;
+            bdd.pocetUzlov++;
             return new Node(input);
 
         }
 
         if(node.left == null && input.length() != 1) {
-            node.left = createNode(node.left, rozdelitStringLava(input));
-            node.right = createNode(node.right, rozdelitStringPrava(input));
-            pocetUzlov++;
+            node.left = createNode(node.left, rozdelitStringLava(input), bdd, height);
+            node.right = createNode(node.right, rozdelitStringPrava(input), bdd,height);
+            //pocetUzlov++;
 
         } if(node.right != null && input.length() != 1){
             System.out.println("splenene");
-            node.left = createNode(node.left, rozdelitStringLava(input));
-            node.right = createNode(node.right, rozdelitStringPrava(input));
-            pocetUzlov++;
+            node.left = createNode(node.left, rozdelitStringLava(input), bdd, height);
+            node.right = createNode(node.right, rozdelitStringPrava(input), bdd, height);
+            //pocetUzlov++;
         }
 
 
@@ -85,26 +93,31 @@ public class BDD {
     }
 
 
-    public char BDD_use(BDD bdd, String vstup){
+    public String BDD_use(BDD bdd, String vstup){
 
-       int vstupLength = vstup.length();
-       int i = 0;
-       char aktChar;//reprezentuje akt char vstupu
-       Node aktNode = bdd.root;
+        int vstupLength = vstup.length();
+        int i = 0;
+        char aktChar;//reprezentuje akt char vstupu
+        Node aktNode = bdd.root;
 
-       while(i < vstupLength){
+        while(i < vstupLength){
             aktChar = vstup.charAt(i);//akt char vo vstupe
             if(aktChar == '1'){
-               aktNode =  aktNode.right;
+                aktNode =  aktNode.right;
 
             }else if(aktChar == '0'){
                 aktNode = aktNode.left;
             }
-           i++;
-       }
+            i++;
+
+        }
 
 
-        return aktNode.key.charAt(0);
+        if(aktNode == null){
+            return "-1";
+        }
+
+        return aktNode.key;//aktNode.key.charAt(0)
     }
 
 
